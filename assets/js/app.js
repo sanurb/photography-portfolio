6d382958b9ca08d5cd5504df09be58b82edbc1ed
SantiagoUrbano <david.urbano@uao.edu.co>
2023-01-10 12:55:34 -0500

    style: :dizzy: Add scroll animations

--- assets/js/app.js
+++ assets/js/app.js
@@ -33,3 +33,26 @@ const swiper = new Swiper(".mySwiper", {
         el: ".swiper-pagination",
     },
 });
+//! Scroll Reveal
+const sr = ScrollReveal({
+    distance: '50px',
+    duration: 1500,
+    easing: 'cubic-bezier(0.68, -0.55, 0.265, 1.55)'
+});
+
+sr.reveal('.showcase__title, .showcase__image, .section-metadata, .swiper', {
+    origin: 'top',
+    interval: 250
+});
+
+sr.reveal('.services__image-wrapper, .about__information, .post--left', {
+    origin: 'left'
+});
+
+sr.reveal('.services__list, .about__images, .post--right', {
+    origin: 'right'
+});
+
+sr.reveal('.footer__container', {
+    origin: 'top'
+});
